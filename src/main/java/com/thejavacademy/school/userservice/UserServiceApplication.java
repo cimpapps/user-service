08f5bc9e5a439987ea5235ed49ba2a102278a88c
catalincimpoeru <catalincimpoeru24@gmail.com>
2020-04-15 22:28:32 +0300

    Initial commit

---
+++ src/main/java/com/thejavacademy/school/userservice/UserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.thejavacademy.school.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thejavacademy/school/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thejavacademy.school.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
